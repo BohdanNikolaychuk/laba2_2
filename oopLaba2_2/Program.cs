e0850a79922512f86ffd21e46f6c6f73b2d28ecd
BohdanNikolaychuk <nikolaychukbm@gmail.com>
2020-04-26 13:29:48 +0300

    1
    laba2_2

---
+++ oopLaba2_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace oopLaba2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|x    |    y=f(x)|");
+            double a=-6, b = 3;
+            double y;
+            double x=a;
+            while(x<b)
+            {
+                y = Math.Pow(x,4) - 12*x+Math.Exp(x);
+             
+                x=x+0.75;
+                Console.WriteLine( x +"\t"+ Math.Round(y));
+            }
+        }
+    }
+}
